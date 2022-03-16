97f9fbe31664dc03faca9517185a124d76cb43d4
Dominik <dmitterer@tsn.at>
2022-03-16 09:30:57 +0100

    Konstruktor implementieren, Methoden aus ZugangsdatenRepo importieren, Implementierung von getAll-Methode

--- src/main/java/ZugangsdatenRepoImpl.java
+++ src/main/java/ZugangsdatenRepoImpl.java
@@ -1,4 +1,58 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class ZugangsdatenRepoImpl implements ZugangsdatenRepository {
 
-}
+    private Connection con;
+
+    public ZugangsdatenRepoImpl() throws SQLException, ClassNotFoundException {
+        this.con = MySqLDBVerbindung.getConnection("jdbc:mysql://localhost:3306/zugangsdaten", "root", "");
+    }
+
+    @Override
+    public Optional<Zugangsdaten> insert(Zugangsdaten entity) {
+        return null;
+    }
+
+
+        @Override
+        public Optional<Zugangsdaten> getById (Long id){
+            return Optional.empty();
+        }
+
+    @Override
+    public List<Zugangsdaten> getAll() {
+
+        String sql = "Select * from `zuganngsdaten`";
+
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ArrayList<Zugangsdaten> zugangsdatenListe = new ArrayList<>();
+            while (resultSet.next()) {
+                zugangsdatenListe.add(new Zugangsdaten(
+                        resultSet.getLong("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("url")
+                ));
+
+            }
+            return zugangsdatenListe;
+        } catch (SQLException e) {
+
+            throw new DatenbankException("Database error occured!");
+        }
+    }
+
+        @Override
+        public List<Zugangsdaten> getAllWithUrlContaining (String urlpart){
+            return null;
+        }
+    }
+
